a312ddcf952af5b88960d3e70c8bfb0364161dd7
LizaTretyakova <elizabet.tretyakova@gmail.com>
2018-01-20 01:18:31 +0300

    saner end screens

--- MyExamApp/app/src/main/java/com/example/liza/myexamapp/Screens/LoseScreen.kt
+++ MyExamApp/app/src/main/java/com/example/liza/myexamapp/Screens/LoseScreen.kt
@@ -8,8 +8,12 @@ class LoseScreen(panelView: AsciiPanelView) : Screen(panelView) {
 
     override fun displayOutput() {
         panel.clear()
-        panel.writeString("You lost.", 1, 1, Color.MAGENTA, null)
-        panel.writeCenter("-- [restart] --", 10, Color.WHITE)
+        panel.writeString("You've lost, young padawan.", 1, 2, Color.YELLOW)
+        panel.writeString("But remember, that patience", 1, 3, Color.YELLOW)
+        panel.writeString("is the truly Jedi way,", 1, 4, Color.YELLOW)
+        panel.writeString("so let the Force guide you,", 1, 5, Color.YELLOW)
+        panel.writeString("and try again.", 1, 6, Color.YELLOW)
+        panel.writeCenter("-- [restart] --", 22)
     }
 
     override fun respondToUserInput(x: Int?, y: Int?, char: AsciiPanelView.ColoredChar): Screen =
--- MyExamApp/app/src/main/java/com/example/liza/myexamapp/Screens/WinScreen.kt
+++ MyExamApp/app/src/main/java/com/example/liza/myexamapp/Screens/WinScreen.kt
@@ -11,7 +11,9 @@ class WinScreen(panelView: AsciiPanelView) : Screen(panelView) {
 
     override fun displayOutput() {
         panel.clear()
-        panel.writeString("You won.", 1, 1, Color.MAGENTA, null)
+        panel.writeString("Today you won, my young apprentice. But be mindful,", 1, 1, Color.YELLOW, null)
+        panel.writeString("and beware of pride, arrogance, and", 1, 2, Color.YELLOW, null)
+        panel.writeString("Disney Studios making a new SW movie every year.", 1, 3, Color.YELLOW, null)
         panel.writeCenter("-- [restart] --", 10, Color.WHITE)
     }
 
